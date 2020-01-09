410f01b9ace74cf7feac35da1f80d409792736e9
hjeffreywang <52841456+hjeffreywang@users.noreply.github.com>
2020-01-08 21:33:23 -0800

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,26 @@
+var aRows1 = [];
+for (var i=1; i<5001; ++i) {
+  aRows1.push('<tr><td>input</td><td>input</td><td>input</td><td>input</td><td>input</td></tr>');
+}
+var clusterize1 = new Clusterize({
+  rows: aRows1,
+  scrollId: 'scroll-area1',
+  contentId: 'content-area1'
+});
+
+var aRows2 = [];
+for (var i=1; i<5001; ++i) {
+  aRows2.push('<tr><td><input></td><td><input></td><td><input></td><td><input></td><td><input></td></tr>');
+}
+var clusterize2 = new Clusterize({
+  rows: aRows2,
+  scrollId: 'scroll-area2',
+  contentId: 'content-area2'
+});
+
+
+
+
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
