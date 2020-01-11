58b62b1db86d228f428610a37a283e46b54b26e4
Nurbol Batkhan <nick@dhcp-v098-138.mobile.uci.edu>
2020-01-11 12:14:28 -0800

    geomapping updated

--- geomapping/Geomaping 2018/world2018.js
+++ geomapping/Geomaping 2018/world2018.js
@@ -15,109 +15,111 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var countries = [
 
   {
-    name: "Finland",
+    name: "#1.Finland",
     location: [61.924110, 25.748152],
-    happiness_score: 7.769
+    happiness_score: 7.632
   },
   {
-    name: "Denmark",
-    location: [56.263920, 9.501785],
-    happiness_score: 7.600
-  },
-  {
-    name: "Norway",
+    name: "#2.Norway",
     location: [60.472023, 8.468946],
-    happiness_score: 7.554
+    happiness_score: 7.594
   },
   {
-    name: "Iceland",
+    name: "#3.Denmark",
+    location: [56.263920, 9.501785],
+    happiness_score: 7.555
+  },
+  {
+    name: "#4.Iceland",
     location: [64.963051, -19.020836],
-    happiness_score: 7.494
+    happiness_score: 7.495
   },
   {
-    name: "Netherlands",
-    location: [52.132633, 5.291266],
-    happiness_score: 7.488
-  },
-  {
-    name: "Switzerland",
+    name: "#5.Switzerland",
     location: [46.818188, 8.227512],
-    happiness_score: 7.480
+    happiness_score: 7.487
   },
   {
-    name: "Sweden",
-    location: [60.128162, 18.643501],
-    happiness_score: 7.343
+    name: "#6.Netherlands",
+    location: [52.132633, 5.291266],
+    happiness_score: 7.441
   },
   {
-    name: "New Zealand",
-    location: [-40.900558, 174.885971],
-    happiness_score: 7.307
-  },
-  {
-    name: "Canada",
+    name: "#7.Canada",
     location: [56.130367, -106.346771],
-    happiness_score: 7.278
+    happiness_score: 7.328
   },
   {
-    name: "Austria",
+    name: "#8.New Zealand",
+    location: [-40.900558, 174.885971],
+    happiness_score: 7.324
+  },
+
+  {
+    name: "#9.Sweden",
+    location: [60.128162, 18.643501],
+    happiness_score: 7.314
+  },
+  {
+    name: "#10.Austria",
     location: [47.516232, 14.550072],
-    happiness_score: 7.246
+    happiness_score: 7.272
+  },
+  //last 10
+  {
+    name: "#147.Malawi",
+    location: [-13.254308, 34.301525],
+    happiness_score: 3.587
   },
   {
-    name: "Haiti",
+    name: "#148.Haiti",
     location: [18.971188, -72.285217],
-    happiness_score: 3.597
+    happiness_score: 3.582
   },
   {
-    name: "Botswana",
-    location: [-22.328474, 24.684866],
-    happiness_score: 3.488
+    name: "#149.Liberia",
+    location: [6.428055, -9.429499],
+    happiness_score: 3.495
   },
   {
-    name: "Syria",
+    name: "#150.Syria",
     location: [34.802074, 38.996815],
     happiness_score: 3.462
   },
   {
-    name: "Malawi",
-    location: [-13.254308, 34.301525],
-    happiness_score: 3.410
-  },
-  {
-    name: "Yemen",
-    location: [15.552727, 48.516388],
-    happiness_score: 3.380
-  },
-  {
-    name: "Rwanda",
+    name: "#151.Rwanda",
     location: [-1.940278, 29.873888],
-    happiness_score: 3.334
+    happiness_score: 3.408
   },
   {
-    name: "Tanzania",
+    name: "#152.Yemen",
+    location: [15.552727, 48.516388],
+    happiness_score: 3.355
+  },
+  {
+    name: "#153.Tanzania",
     location: [-6.369028, 34.888821],
-    happiness_score: 3.231
+    happiness_score: 3.303
   },
   {
-    name: "Afghanistan",
-    location: [33.939110, 67.709953],
-    happiness_score: 3.203
+    name: "#154.South Sudan",
+    location: [6.876992, 31.306978],
+    happiness_score: 3.254
   },
   {
-    name: "Central African Republic",
+    name: "#155.Central African Republic",
     location: [6.611111, 20.939444],
     happiness_score: 3.083
   },
   {
-    name: "South Sudan",
-    location: [6.876992, 31.306978],
-    happiness_score: 2.853
-  }
+    name: "#156.Burundi",
+    location: [-3.373056, 29.918886],
+    happiness_score: 2.905
+  } 
 
 ];
 
-// Loop through the countries array and create one marker for each city object
+// Loop through the countries array and create one marker for each country object
 for (var i = 0; i < countries.length; i++) {
 
   // Conditionals for countries points
